67075724d86b9abc35b68052d90e66a5bd6d0c7a
DEVANSH-DOGRA <101620372+DEVANSH-DOGRA@users.noreply.github.com>
2022-05-31 14:18:42 +0530

    Update Sum.cpp
    CODE APPENDED TO INPUT 4 ELEMENTS AND  PRINT THEIR SUM USING ARRAYS!!!

--- Sum.cpp
+++ Sum.cpp
@@ -8,6 +8,17 @@ cin >> firstNumber >> secondNumber;
 // sum of two numbers in stored in variable sumOfTwoNumbers
 sumOfTwoNumbers = firstNumber + secondNumber;
 // Prints sum
-cout << firstNumber << " + " << secondNumber << " = " << sumOfTwoNumbers;
+cout << firstNumber << " + " << secondNumber << " = " << sumOfTwoNumbers<< endl;
+  //I AM DEVANSH. I AM APPENDING A CODE THAT TAKES 4 ARRAY ELEMENTS AS INPUT AND THEN PRINTS THEIR SUM !!!!!!!!
+  int arr[4];
+  int sum=0;
+  cout<<"Enter 4 elements to sum up:"<<endl;
+  for(int i=0;i<=3;i++)
+  {
+    cin>>arr[i];
+    sum=sum+arr[i];
+    
+  }
+  cout<<"Sum of the entered elements is: "<<sum<<endl;
 return 0;
 }
